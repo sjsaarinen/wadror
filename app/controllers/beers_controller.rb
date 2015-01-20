ef1714f99f9a47809b2ad39b3f66e57d7f600530
Sami-Joonas <Sami Saarinen>
2015-01-20 14:05:37 +0200

    Tehtävät 1-9 tehty

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -15,6 +15,9 @@ class BeersController < ApplicationController
   # GET /beers/new
   def new
     @beer = Beer.new
+    @breweries = Brewery.all
+    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter"]
+
   end
 
   # GET /beers/1/edit
@@ -28,13 +31,14 @@ class BeersController < ApplicationController
 
     respond_to do |format|
       if @beer.save
-        format.html { redirect_to @beer, notice: 'Beer was successfully created.' }
-        format.json { render :show, status: :created, location: @beer }
+        format.html { redirect_to beers_path, notice: 'Beer was successfully created.' }
+        format.json { render :show, status: :created, location: beers_path }
       else
         format.html { render :new }
         format.json { render json: @beer.errors, status: :unprocessable_entity }
       end
     end
+
   end
 
   # PATCH/PUT /beers/1
