1cd946ce0d739f83466dd9ac7260598a7edc2511
Sami Saarinen <sjsaarinen@gmail.com>
2015-01-31 22:34:34 +0200

    vko3 1-5 tehty

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,6 +3,8 @@ class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
 
+  validates :name, presence: true
+
   def to_s
     "#{brewery.name}: #{name}"
   end
