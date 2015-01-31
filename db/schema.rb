e6407a07f46bf6e91706d87f43eae06126b5f0bf
Sami Saarinen <sjsaarinen@gmail.com>
2015-01-31 08:25:31 +0200

    vko3 1-3 tehty

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126211335) do
+ActiveRecord::Schema.define(version: 20150131060300) do
 
   create_table "beers", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150126211335) do
     t.integer  "beer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
