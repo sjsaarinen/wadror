8c4d0372c0ad66182ed488c83dba0936237fd802
Sami Saarinen <sjsaarinen@gmail.com>
2015-01-31 17:30:23 +0200

    vko3 1-4 tehty

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -10,11 +10,14 @@ class RatingsController < ApplicationController
 
   def create
     rating = Rating.create params.require(:rating).permit(:score, :beer_id)
-
-    # talletetaan tehdyn reittauksen sessioon
-    session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
-
+    current_user.ratings << rating
     redirect_to ratings_path
+  end
+
+  def destroy
+    rating = Rating.find(params[:id])
+    rating.delete
+    redirect_to :back
   end
 
 end
