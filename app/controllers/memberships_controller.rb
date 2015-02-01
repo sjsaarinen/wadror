0ef6131a923bbaaa4948f8154a61a676f6ae77dc
Sami Saarinen <sjsaarinen@gmail.com>
2015-02-01 13:46:47 +0200

    vko3 1-9 tehty

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -35,6 +35,7 @@ class MembershipsController < ApplicationController
         format.html { redirect_to @membership, notice: 'Membership was successfully created.' }
         format.json { render :show, status: :created, location: @membership }
       else
+        @beer_clubs = BeerClub.all
         format.html { render :new }
         format.json { render json: @membership.errors, status: :unprocessable_entity }
       end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,7 @@
 class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :beer_club
+
+  validates :user_id, uniqueness: { scope: :beer_club_id, message: "is already a member"}
+
 end
