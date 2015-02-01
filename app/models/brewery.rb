8932e0baf76a291154cc0424381f4c99eb942b00
Sami Saarinen <sami.j.saarinen@helsinki.fi>
2015-02-01 12:27:41 +0200

    vko3 1-6 tehty

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,7 +5,7 @@ class Brewery < ActiveRecord::Base
 
   validates :name, presence: true
   validates :year, numericality: { greater_than_or_equal_to: 1042,
-                                   less_than_or_equal_to: 2015,
+                                   less_than_or_equal_to: lambda {|_| Time.now.year},
                                    only_integer: true }
 
   def print_report
