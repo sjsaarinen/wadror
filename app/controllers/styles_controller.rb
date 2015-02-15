6df443aea44c1c35f02f4fa37889bc98a925fd1c
Sami Saarinen <sjsaarinen@gmail.com>
2015-02-16 00:06:21 +0200

    form for adding styles

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -8,4 +8,29 @@ class StylesController < ApplicationController
     @style = Style.find(params[:id])
   end
 
+  # GET /styles/new
+  def new
+    @style = Style.new
+  end
+
+  # POST /styles
+  # POST /styles.json
+  def create
+    @style = Style.new(style_params)
+
+    respond_to do |format|
+      if @style.save
+        format.html { redirect_to styles_path, notice: 'Style was successfully created.' }
+        format.json { render :show, status: :created, location: @style }
+      else
+        format.html { render :new }
+        format.json { render json: @style.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def style_params
+    params.require(:style).permit(:name, :description)
+  end
+
 end
