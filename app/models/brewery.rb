54dda29451c52e5593159f851a822ed3beebdbff
Sami Saarinen <sjsaarinen@gmail.com>
2015-02-18 08:43:39 +0200

    vko5 1-4 tehty

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,6 +5,9 @@ class Brewery < ActiveRecord::Base
   validates :year, numericality: { greater_than_or_equal_to: 1024,
                                    less_than_or_equal_to: ->(_){Time.now.year} }
 
+  scope :active, -> { where active:true }
+  scope :retired, -> { where active:[nil,false] }
+
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 end
