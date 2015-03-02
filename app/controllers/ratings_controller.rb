bc90b766e9a14c7be12bdb1ebdc986bc8f1de264
Sami Saarinen <sjsaarinen@gmail.com>
2015-03-02 17:15:35 +0200

    vko7 1-5 9-12 tehty

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,8 +1,26 @@
 class RatingsController < ApplicationController
   def index
-    @ratings = Rating.all
-    @recent_ratings = Rating.recent
+    return if fragment_exist? 'ratings_cache'
+
+    @top_beers = Beer.top(3)
+    @top_breweries = Brewery.top(3)
+    @top_styles = Style.top(3)
     @top_raters = User.top_raters(3)
+    @recent_ratings = Rating.recent
+    @ratings = Rating.all
+
+    #Rails.cache.write('beer top 3', Beer.top(3)) unless fragment_exist? 'ratings_cache'
+    #@top_beers = Rails.cache.read 'beer top 3'
+    #Rails.cache.write('brewery top 3', Brewery.top(3)) unless fragment_exist? 'ratings_cache'
+    #@top_breweries = Rails.cache.read 'brewery top 3'
+    #Rails.cache.write('style top 3', Style.top(3)) unless fragment_exist? 'ratings_cache'
+    #@top_styles = Rails.cache.read 'style top 3'
+    #Rails.cache.write('rater top 3', User.top_raters(3)) unless fragment_exist? 'ratings_cache'
+    #@top_raters = Rails.cache.read 'rater top 3'
+    #Rails.cache.write('recent ratings', Rating.recent) unless fragment_exist? 'ratings_cache'
+    #@recent_ratings = Rails.cache.read 'recent ratings'
+    #Rails.cache.write('all ratings', Rating.all) unless fragment_exist? 'ratings_cache'
+    #@ratings = Rails.cache.read 'all ratings'
   end
 
   def new
